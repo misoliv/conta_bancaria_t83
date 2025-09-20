34bd1b8d736a0221ef42d2e79e1425f1c13beb81
Milena <milenasvitras@gmail.com>
2025-09-20 15:58:28 -0300

    Commit Menu e Cores

--- src/conta_bancaria/util/Cores.java
+++ src/conta_bancaria/util/Cores.java
@@ -2,4 +2,16 @@ package conta_bancaria.util;
 
 public class Cores {
 
+		//Reset Color
+		public static final String TEXT_RESET = "\u001B[0m";
+
+		//Text Color
+		public static final String TEXT_CYAN = "\u001B[36m";
+		
+		//Text Color Bold
+		public static final String TEXT_WHITE_BOLD = "\033[1;37m";
+	
+		// Background Color
+		public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+				
 }
